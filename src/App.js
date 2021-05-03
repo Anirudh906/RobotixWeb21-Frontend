d27cf402792d66e54fbce843b26c5423cb1a1ea1
Gaurav Dhruw <dhruwgaurav01@gmail.com>
2021-05-03 18:21:48 +0530

    main and aboutus components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import AboutUs from "./components/AboutUs";
+import Gallery from "./components/Gallery";
+import Main from "./components/Main"
+
+
 
 function App() {
   return (
     <div>
-     check
+     
     </div>
   );
 }
