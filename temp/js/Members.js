5f0d90e794ab43cac1a3c14bd88d2848f2c0900f
Anirudh906 <40207836+Anirudh906@users.noreply.github.com>
2021-05-10 12:45:12 +0530

    update temp

---
+++ temp/js/Members.js
@@ -0,0 +1,52 @@
+function underline1(){
+    const buttons = document.querySelectorAll('.btn-primary');
+    buttons.forEach((button)=>{
+        button.style.border = '3px solid #e14eca';
+    });
+    document.getElementById('btn1').style.border = '3px solid white';
+    
+    const bottoms = document.querySelectorAll('.bottom');
+    bottoms.forEach((bottom)=>{
+        bottom.style.display = 'none';
+    });
+    document.getElementById('BotOne').style.display = 'Block';
+}
+function underline2(){
+    const buttons = document.querySelectorAll('.btn-primary');
+    buttons.forEach((button)=>{
+        button.style.border = '3px solid #e14eca';
+    });
+    document.getElementById('btn2').style.border = '3px solid white';
+
+    const bottoms = document.querySelectorAll('.bottom');
+    bottoms.forEach((bottom)=>{
+        bottom.style.display = 'none';
+    });
+    document.getElementById('BotTwo').style.display = 'Block';
+}
+function underline3(){
+    const buttons = document.querySelectorAll('.btn-primary');
+    buttons.forEach((button)=>{
+        button.style.border = '3px solid #e14eca';
+    });
+    document.getElementById('btn3').style.border = '3px solid white';
+
+    const bottoms = document.querySelectorAll('.bottom');
+    bottoms.forEach((bottom)=>{
+        bottom.style.display = 'none';
+    });
+    document.getElementById('BotThree').style.display = 'Block';
+}
+function underline4(){
+    const buttons = document.querySelectorAll('.btn-primary');
+    buttons.forEach((button)=>{
+        button.style.border = '3px solid #e14eca';
+    });
+    document.getElementById('btn4').style.border = '3px solid white';
+
+    const bottoms = document.querySelectorAll('.bottom');
+    bottoms.forEach((bottom)=>{
+        bottom.style.display = 'none';
+    });
+    document.getElementById('BotFour').style.display = 'Block';
+}
