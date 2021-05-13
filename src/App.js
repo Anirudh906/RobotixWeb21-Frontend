bd30c8473720b132502f4ff58ce3946c3d4ea725
Jay Prakash <50571500+jayprakash02@users.noreply.github.com>
2021-05-13 16:18:47 +0530

    Revert "Dev"

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React from "react";
 import AboutUs from "./components/AboutUs";
 import Gallery from "./components/Gallery";
 import Main from "./components/Main"
-import Vanta from "./components/Test2"
-import VantaR from "./components/Test1";
-import Events from "./components/Events";
+
+
+
 function App() {
   return (
     <div>
-     
-     <VantaR />
+     <Main />
      <AboutUs />
      <Gallery />
-     
-     <Events />
     </div>
   );
 }
